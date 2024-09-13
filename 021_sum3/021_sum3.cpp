81f9ee617bfaa533602b6dd13a80873b12767659
ljy0711 <wjddus5696@naver.com>
2024-09-13 15:12:19 +0900

    Add files via upload

---
+++ 021_sum3/021_sum3.cpp
@@ -0,0 +1,29 @@
+﻿// 021_sum3.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	int n;  //변수 선언
+	int a=0; // 숫자
+	int b=0; //홀수
+	int c=0; //짝수
+
+	printf("n을 입력하시오 :");
+	scanf_s("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		a += i;
+		if (i % 2 == 0)
+			c += i;
+		else
+			b += i;
+	}
+	printf("숫자의 합: %d\n", a);
+	printf("홀수의 합: %d\n", b);
+	printf("짝수의 합: %d\n", c);
+
+	return 0;
+}
+
---
+++ 022_gugudan/022_gugudan.cpp
@@ -0,0 +1,18 @@
+﻿// 022_gugudan.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	int n;
+
+	printf("단 입력:");
+	scanf_s("%d", &n);
+
+	for (int i = 1; i <= 9; i++) {
+		printf("%d X %d = %d\n", n, i, i*n);
+	} 
+	return 0;
+}
+
