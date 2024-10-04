a4aa5d7d0faf70d6835e00bced030c11c2977592
ljy0711 <wjddus5696@naver.com>
2024-10-04 17:56:51 +0900

    Add files via upload

---
+++ 024_triangle/024_triangle.cpp
@@ -0,0 +1,18 @@
+﻿// 024_triangle.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("자연수 n을 입력: ");
+    scanf_s("%d", &n);
+
+    for (int i = 1; i <= 4; i++) {
+        for (int j = 1; j <= 2 * i - 1; j++)
+            printf("%d", j);
+        printf("\n");
+    }
+}
+
---
+++ 025_divider/025_divider.cpp
@@ -0,0 +1,18 @@
+﻿// 025_divider.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    
+    printf("n 입력:");
+    scanf_s("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+        if (n % 1 == 0)
+            printf("%d", i);
+    printf("\n");
+}
+
