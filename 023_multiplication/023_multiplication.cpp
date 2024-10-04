2c106d1fe68b415d330c41a226cd1e9689ae3316
ljy0711 <wjddus5696@naver.com>
2024-10-04 17:19:00 +0900

    Add files via upload

---
+++ 023_multiplication/023_multiplication.cpp
@@ -0,0 +1,22 @@
+﻿// 023_multiplication.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    for (int j = 1; j <= 9; j++) {
+        for (int i = 2; i <= 5; i++)
+            printf("%d x %d = %d\t", i, j, i * j);
+            printf("\n");
+    }
+    printf("\n");
+
+    for (int j = 1; j <= 9; j++) {
+        for (int i = 6; i <= 9; i++)
+            printf("%d x %d = %d\t", i, j, i * j);
+            printf("\n");
+    }
+    
+}
+
